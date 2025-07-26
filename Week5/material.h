b9fa9f0f32a17ba20a4c176872060c0c6e72d525
i-am-batpool <advait.physics@gmail.com>
2025-07-26 13:07:26 +0000

    CPU track done

--- Week5/material.h
+++ Week5/material.h
@@ -45,4 +45,33 @@ class metal : public material {
         }
 };
 
+class dielectric : public material {
+    private:
+        real refractive_index;
+
+        static double reflectance(double cosine, double refraction_index) {//Schlick's approximation 
+            auto r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0*r0;
+            return r0 + (1-r0)*std::pow((1 - cosine),5);
+        }
+
+    public:
+        dielectric(double refractive_index): refractive_index(refractive_index) {}
+
+        bool scatter(const ray& in, const hit_record& rec, color& attenuation, ray& scattered) const override{
+            attenuation=color(1.0,1.0,1.0);
+            Vec3 scatter_dir;
+
+            real ref = rec.front_face?refractive_index:1.0/refractive_index;
+
+            real cos_theta = std::fmin(-in.direction().unit().dot(rec.normal), 1.0);
+            if (reflectance(cos_theta,ref)>random_real()){
+                scatter_dir = in.direction().reflect(rec.normal);
+            } else scatter_dir = in.direction().refract(rec.normal, ref);
+            
+            scattered=ray(rec.point, scatter_dir);
+            return true;
+        }
+};
+
 #endif
